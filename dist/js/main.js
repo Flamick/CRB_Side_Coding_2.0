648df9b71daa6453880f2c70985e4ed2415b68f6
Flamick <adhd_ninja7@Yahoo.com>
2021-09-01 21:38:17 -0400

    Overhaul Update

---
+++ dist/js/main.js
@@ -0,0 +1,36 @@
+//* Hamburger menu
+const menuBtn = document.querySelector('.menu-btn');
+const mobileMenu = document.querySelector('#mobile-menu');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+if(!menuOpen) {
+    menuBtn.classList.add('open');
+    mobileMenu.classList.add('open');
+    menuOpen = true;
+} else {
+    menuBtn.classList.remove('open');
+    mobileMenu.classList.remove('open');
+    menuOpen = false;
+}
+});
+
+//* Click events toggle
+let itemOverlay = Array.from(document.querySelectorAll('.item'));
+
+const clickEffect = (e) => {
+    if(mediaDevice.matches) {
+        e.preventDefault();
+        clicked = document.querySelector('.item.clicked');
+        const unclicked = document.querySelectorAll('.item');
+        if(unclicked){
+            e.currentTarget.classList.add('clicked');
+            clicked.classList.remove('clicked');
+        }
+    }
+}
+
+itemOverlay.forEach(node => {
+    node.addEventListener('click', clickEffect)
+});
+
+const mediaDevice = window.matchMedia('(max-width: 799px)');
